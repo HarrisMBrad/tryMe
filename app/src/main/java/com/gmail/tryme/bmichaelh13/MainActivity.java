3f06776f9af9e192060271bb3f92dabea1cfd430
HarrisMBrad <bmichaelh13@gmail.com>
2020-03-14 22:47:21 -0400

    Initial Commit

---
+++ app/src/main/java/com/gmail/tryme/bmichaelh13/MainActivity.java
@@ -0,0 +1,55 @@
+package com.gmail.tryme.bmichaelh13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View windowView;
+    private Button tryMeButton;
+    private int[] colors;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        colors = new int[]{Color.DKGRAY,Color.CYAN, Color.GRAY, Color.RED, Color.BLUE, Color.BLACK, Color.LTGRAY,Color.MAGENTA,Color.YELLOW};
+
+        windowView = findViewById(R.id.windowViewId);
+
+
+        tryMeButton = (Button) findViewById(R.id.tryMeButtonId);
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Button logic
+
+                int colorArrayLength = colors.length;
+
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+
+                windowView.setBackgroundColor(colors[randomNum]);
+
+                Log.d("Random", String.valueOf(randomNum));
+
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
